df6f1b78b9aa7c99e50a4d0e805f91877a336347
Bruce Johnson <bruce@yogainternational.com>
2020-03-31 13:10:52 -0400

    adds react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Homepage } from "./Homepage";
 
 const App = () => (
   <div style={{ textAlign: "center", margin: "auto" }}>
-    <Homepage />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 
