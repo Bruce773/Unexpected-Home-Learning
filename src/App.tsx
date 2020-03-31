afdcaf4f63c79e9da16f97daa36c02713ef4b2c0
Bruce Johnson <bruce@yogainternational.com>
2020-03-31 13:00:22 -0400

    adds initial project files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const App = () => (
+  <div style={{ textAlign: "center", margin: "auto" }}>
+    <Typography variant="h2">Unexpected Home Learning</Typography>
+  </div>
+);
+
+export default App;
