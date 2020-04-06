7a75f333b42399c70766a4eb2134e773af9070cc
Bruce Johnson <bruce@yogainternational.com>
2020-04-06 16:28:34 -0400

    adds styling improvements to /live-comments

--- src/LiveComments/LiveComments.tsx
+++ src/LiveComments/LiveComments.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import { MainHeader } from "Components";
+import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import styled from "styled-components";
+import { BaseFont, tealGreen } from "globalStyles";
+
+const SubText = styled(BaseFont)`
+  && {
+    font-size: 1.6rem;
+    color: ${tealGreen};
+  }
+`;
 
 export const LiveComments: React.FC = () => (
   <>
+    <Link to="/">
+      <Paper style={{ position: "fixed", top: "0px", marginLeft: "35px" }}>
+        <Button variant="text">
+          <ArrowBackIcon /> Home
+        </Button>
+      </Paper>
+    </Link>
     <MainHeader variant="h2">Unexpected Home Learning</MainHeader>
+    <SubText>Share your homeschooling experience with others</SubText>
     <Container maxWidth="md" style={{ paddingTop: "60px" }}>
       <div id="disqus_thread"></div>
     </Container>
